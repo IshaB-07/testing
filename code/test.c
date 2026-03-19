fce45460e7712fc5e8da155a1060452b08038647
Isha Burange <113483305+IshaB-07@users.noreply.github.com>
2026-03-19 13:43:45 -0400

    Create test.c

---
+++ code/test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Global variable
+int shared_counter = 0;
+
+// Function to increment the global variable
+void increment() {
+    shared_counter++;
+    printf("Incremented: %d\n", shared_counter);
+}
+
+// Function to decrement the global variable
+void decrement() {
+    shared_counter--;
+    printf("Decremented: %d\n", shared_counter);
+}
+
+int main() {
+    printf("Initial value: %d\n", shared_counter);
+
+    // Call functions (no threading yet)
+    increment();
+    increment();
+    decrement();
+    increment();
+    decrement();
+
+    printf("Final value: %d\n", shared_counter);
+
+    return 0;
+}
